1db089117a3da9a37a4ebc5ae4a858cba9286727
XPert007 <cookingcapybara@proton.me>
2026-01-31 01:34:24 +0530

    added models

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use anyhow::{self, Ok};
+mod model;
+mod subdomain;
+use reqwest::Client;
+use thiserror::Error;
+#[derive(Error, Debug, Clone)]
+
+pub enum Error {
+    #[error("Usage: tricoder <Domain>")]
+    CliUsage,
+}
+
+fn main() -> Result<(), anyhow::Error> {
     println!("Hello, world!");
+    Ok(())
 }
